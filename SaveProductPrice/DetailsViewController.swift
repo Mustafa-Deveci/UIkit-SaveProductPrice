d25417af7a68a737534917b09ec5ab2b4dda2472
md <>
2023-08-20 10:29:20 +0300

    data recording to core data, data extraction operations were done.

--- SaveProductPrice/DetailsViewController.swift
+++ SaveProductPrice/DetailsViewController.swift
@@ -6,16 +6,72 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
+    @IBOutlet weak var SaveButton: UIButton!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
+    var SelectedProductName = ""
+    var SelectedProductUUID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if SelectedProductName != ""  {
+            
+            SaveButton.isHidden = true
+            
+            if let uuidString = SelectedProductUUID?.uuidString {
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                
+                do{
+                    let results = try context.fetch(fetchRequest)
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject] {
+                            
+                            if let name = result.value(forKey: "name") as? String {
+                                nameTextField.text = name
+                            }
+                            
+                            if let price = result.value(forKey: "price") as? Int {
+                                priceTextField.text = (String)(price)
+                            }
+                            
+                            if let date = result.value(forKey: "date") as? Date {
+                                datePicker.date = date
+                            }
+                            
+                            if let imageData = result.value(forKey: "image") as? Data {
+                                let image = UIImage(data: imageData)
+                                imageView.image = image
+                            }
+                        }
+                    }
+                }catch {
+                    print("error")
+                }
+            }
+            
+        } else {
+            SaveButton.isHidden = false
+            SaveButton.isEnabled = false
+            nameTextField.text = ""
+            priceTextField.text = ""
+            let currentDate = Date()
+            datePicker.date = currentDate
+        }
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector (closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
@@ -40,12 +96,40 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate &
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.editedImage] as? UIImage
+        SaveButton.isEnabled = true
         self.dismiss(animated: true, completion: nil)
     }
     
     
     @IBAction func saveButton(_ sender: Any) {
         
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let product = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context)
+        
+        product.setValue(nameTextField.text!, forKey: "name")
+        product.setValue(datePicker.date, forKey: "date")
+        
+        if let price = Int(priceTextField.text!) {
+            product.setValue(price, forKey: "price")
+        }
+        
+        product.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.7)
+        product.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("saved")
+        }catch {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "addedData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     
 }
+
+
--- SaveProductPrice/ViewController.swift
+++ SaveProductPrice/ViewController.swift
@@ -6,24 +6,137 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var arrName = [String]()
+    var arrId = [UUID]()
+    var selectedName = ""
+    var selectedUUID : UUID?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        getData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "addedData"), object: nil)
     }
     
     @objc func addButton() {
-        
+        selectedName = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
     }
     
+    @objc func getData()  {
+        
+        arrId.removeAll()
+        arrName.removeAll()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        arrName.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        arrId.append(id)
+                    }
+                    
+                }
+                tableView.reloadData()
+                
+            }
+            
+        } catch {
+            print("an error")
+            
+            
+        }
+    }
     
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = arrName[indexPath.row]
+        return cell
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsViewController
+            destinationVC.SelectedProductName = selectedName
+            destinationVC.SelectedProductUUID = selectedUUID
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedName = arrName[indexPath.row]
+        selectedUUID = arrId[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle:UITableViewCell.EditingStyle,forRowAt indexPath: IndexPath){
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
+            let uuidString = arrId[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == arrId[indexPath.row]{
+                                context.delete(result)
+                                arrName.remove(at: indexPath.row)
+                                arrId.remove(at: indexPath.row)
+                                
+                                self.tableView.reloadData()
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print("error")
+                                    
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+                tableView.reloadData()
+                
+            } catch {
+                print("an error")
+            }
+        }
+    }
 }
+
 
